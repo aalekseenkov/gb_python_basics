eb437ca53cee0b468bf9b17a57071b6019959381
Andrew Alekseenkov <andrew.alekseenkov@yandex.by>
2022-10-09 23:48:41 +0300

    add lesson_1/task_6

---
+++ Lesson_1/task_6.py
@@ -0,0 +1,21 @@
+"""
+Задание 6.
+
+Если фирма отработала с прибылью, вычислите рентабельность выручки.
+Это отношение прибыли к выручке. Далее запросите численность сотрудников фирмы
+и определите прибыль фирмы в расчёте на одного сотрудника.
+Прибыль фирмы в расчете на одного сотрудника = 50.0
+"""
+gain = int(input("Enter gain of your company: "))
+cost = int(input("Enter cost of your company: "))
+if gain > cost:
+    profit = gain - cost
+    print(f"The financial result is PROFIT! The value of {profit}...")
+    profitability = profit / gain
+    print(f"The profitability of your company is {profitability}.")
+    employees = int(input("Enter your employees' number: "))
+    profit_for_person = profit / employees
+    print(f"The company's profit per employee is {profit_for_person}.")
+else:
+    loss = cost - gain
+    print(f"The financial result is LOSS! The value of {loss}...")
