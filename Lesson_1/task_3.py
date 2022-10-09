e925277fa34669b3a8c5161656e6ac63dfd5bf52
Andrew Alekseenkov <andrew.alekseenkov@yandex.by>
2022-10-09 20:18:26 +0300

    fix lesson_1/task_3

--- Lesson_1/task_3.py
+++ Lesson_1/task_3.py
@@ -1,10 +1,11 @@
 """
 Задание 3.
 
-Узнайте у пользователя число n. 
+Узнайте у пользователя число n.
 Найдите сумму чисел n + nn + nnn.
+Например, пользователь ввёл число 3.
+Считаем 3 + 33 + 333 = 369.
 """
-n = int(input("Enter integer number n: "))
-result = n + n**2 + n**3
+n = input("Enter integer number n: ")
+result = int(n) + int(n+n) + int(n+n+n)
 print(f"n + nn + nnn = {result}")
-
