72057d3550755392ccc035a447cb73d146e4326c
Andrew Alekseenkov <andrew.alekseenkov@yandex.by>
2022-10-11 20:45:52 +0300

    fix pep-8 errors in lesson_1/task_4

--- Lesson_1/task_4.py
+++ Lesson_1/task_4.py
@@ -6,10 +6,10 @@
 Для решения используйте цикл while и только арифметические операции.
 """
 num = int(input("Enter an integer: "))
-max = num % 10
+max_digit = num % 10
 num = num // 10
 while num > 0:
-    if num % 10 > max:
-        max = num % 10
+    if num % 10 > max_digit:
+        max_digit = num % 10
     num = num // 10
-print(f"The largest digit is {max}")
+print(f"The largest digit is {max_digit}")
